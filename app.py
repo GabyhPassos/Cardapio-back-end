9cc8fd855c0e34cf66fab716fdf88bede7262b8d
GabyhPassos <gabrielapassossilva@gmail.com>
2024-08-17 05:33:05 -0300

    criação de arquivo

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def index():
+def homepage():
     return {
-        "msg": "hello world"
+        render_template("homepage.html"),
     }
 
+@app.route("/contatos")
+def contatos():
+    return { 
+        render_template("contatos.html"),
+    }
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8081)
+@app.route("/usuarios/<nome_usuario>")
+def usuarios(nome_usuario):
+    return {
+        render_template("usuarios.html", nome_usuario=nome_usuario),
+            }
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
